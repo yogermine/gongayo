4e6ef2fd13d6f61914081c75c914cbf181c9569d
yogermine <yogermine@gmail.com>
2026-04-20 19:35:31 +0900

    시험문제 제공추가

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+/*
 export default defineConfig({
   plugins: [react()],
+})
+*/
+export default defineConfig({
+  build: {
+    outDir: 'dist', // Vercel이 찾고 있는 이름과 일치시키기
+  }
 })

cf6a330328b65db66f15d15937e72d7836435332
yogermine <yogermine@gmail.com>
2026-06-11 15:42:04 +0900

    Fix: merge duplicate vite config

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-/*
+
 export default defineConfig({
   plugins: [react()],
-})
-*/
-export default defineConfig({
   build: {
-    outDir: 'dist', // Vercel이 찾고 있는 이름과 일치시키기
-  }
-})
-export default defineConfig({
-  plugins: [react()],
+    outDir: 'dist',
+  },
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001', // 백엔드 서버 주소  
-        changeOrigin: true, 
+        target: 'http://localhost:3001',
+        changeOrigin: true,
       }
     }
   }

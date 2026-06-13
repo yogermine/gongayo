0e9a6acac2468fb9695f6246f34e040b8ed3554b
yogermine <yogermine@gmail.com>
2026-06-13 14:35:18 +0900

    db연결 수정

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        app: resolve(__dirname, 'app.html'),
       }
     }
   }
